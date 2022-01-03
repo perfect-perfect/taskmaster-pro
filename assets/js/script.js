0ea835e196b2a5af25b5c28511247331dc2e5e34
perfect perfect <gilberto.j.covarrubias@gmail.com>
2022-01-03 02:50:21 -0500

    finsihed 5.1 and 5.2

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,6 @@ var loadTasks = function() {
 
   // loop over object properties
   $.each(tasks, function(list, arr) {
-    console.log(list, arr);
     // then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
@@ -45,8 +44,120 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+// this and the folliwng function are to edit and update task text
+$(".list-group").on("click", "p", function() {
+  var text = $(this)
+    .text()
+    .trim();
+    
+
+  //  using <> here makes jQuery create an element
+  var textInput = $("<textarea>")
+    .addClass("form-control")
+    .val(text);
+  
+  $(this).replaceWith(textInput);
+
+  textInput.trigger("focus");
+  
 
 
+});
+
+// the blur event will trigger as soon as the user interacts with anything other then the <textarea> elememt. so, after you are done editing you just clcik away from the text area and it will run this
+$(".list-group").on("blur", "textarea", function(){
+  // get the textarea's current value/text
+  var text = $(this)
+    .val()
+    .trim();
+  
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+
+    // returns the ID, which will be "list-" followed by a catagory. 
+    // we are chaining this operator to replace() to remove "list-" from the text, which will give us the catagory name (e.g., "To Do") that will match one of the arrays on the tasks object(e.g., tasks.toDo)
+    .attr("id")
+
+    // js operator to find and replace text in a string
+    .replace("list-", "");
+
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // returns the text property of th object at a given index
+  tasks[status][index].text = text;
+
+  // updating this tasks object was necesarry for localStorage, so we call saveTasks()
+  saveTasks();
+
+  var taskP = $("<p>")
+    .addClass("m-1")
+    .text(text);
+
+  // replace textarea with p element
+  $(this).replaceWith(taskP);
+
+});
+
+// this and the next function update dates in the tasks
+// due date was clicked
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this)
+    .text()
+    .trim();
+
+  // crea new input element
+  var dateInput = $("<input>")
+    // with two attributes listed the attri() operator creates and sets and attribute
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+
+  // swap out elements
+  $(this).replaceWith(dateInput);
+
+  // automatically focus on new element
+  dateInput.trigger("focus");
+});
+
+// one they click outside of the date after editing, this will run.
+// value of due date has changed
+$(".list-group").on("blur", "input[type='text']", function() {
+  // get current text
+  var date = $(this)
+    .val()
+    .trim();
+
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "")
+
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // update tas in array and re-save to localStorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  //reacreate span element with bootstap classes
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+  // replace input with span element
+  $(this).replaceWith(taskSpan);
+
+});
+
+ 
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
